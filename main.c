89d59af888a9fb2fd4abffa1c65b4283c85f5644
NadaaSanad <san222026@gmail.com>
2024-03-14 23:38:19 +0200

    track it

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * graduation project.c
+ *
+ * Created: 14/03/2024 11:17:07 م
+ * Author : kk
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+	DDRA=&(1<<2);
+    while (1) 
+    {
+    }
+}
+

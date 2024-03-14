3549a5f19f1a31aa0fe704fc6986b5f53c48d63a
NadaaSanad <san222026@gmail.com>
2024-03-14 23:40:31 +0200

    tracking them

---
+++ adc.h
@@ -0,0 +1,16 @@
+﻿/*
+ * adc.h
+ *
+ * Created: 14/03/2024 11:18:33 م
+ *  Author: kk
+ */ 
+
+
+#ifndef ADC_H_
+#define ADC_H_
+
+
+
+
+
+#endif /* ADC_H_ */
---
+++ mq2.h
@@ -0,0 +1,16 @@
+﻿/*
+ * mq2.h
+ *
+ * Created: 14/03/2024 11:17:53 م
+ *  Author: kk
+ */ 
+
+
+#ifndef MQ2_H_
+#define MQ2_H_
+
+
+
+
+
+#endif /* MQ2_H_ */
